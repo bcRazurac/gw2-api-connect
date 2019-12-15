62f16a4345bcb3f2a7ab3c4213b2cdd12a83682f
Razurac <azelch@r4zur4c-srv.eu>
2019-12-15 13:06:43 +0100

    Update setup.py
    Changed out links and authors

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@
 from setuptools import setup, find_packages
 
 __version__ = '0.5.7'
-REPOSITORY = 'https://github.com/JuxhinDB/gw2-api-interface'
+REPOSITORY = 'https://github.com/razurac/gw2-connector'
 
 with open('README.rst', 'r') as f:
     README = f.read()
 
 
 setup(
-    name='GuildWars2-API-Client',
+    name='GuildWars2-Connector',
     version=__version__,
     description='Library that interfaces with the Guild Wars 2 API that supports v1 and v2',
     long_description=README,
-    author='JDB',
-    author_email='JuxhinBox@gmail.com',
+    author='Razurac',
+    author_email='azelch@r4zur4c-srv.eu',
     url=REPOSITORY,
     packages=find_packages(exclude=['test', 'res']),
     install_requires=[
